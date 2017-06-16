4c5a01ef7128b2cbf02a27dbd9c889912c9c1818
schuer <ds@decaf.de>
2017-06-16 23:34:29 +0200

    CS

--- docker/php-apache/docker-entrypoint.sh
+++ docker/php-apache/docker-entrypoint.sh
@@ -18,23 +18,23 @@ if isempty "$PWD"; then
     # extract redaxo package
     unzip -oq /tmp/redaxo5.zip
     rm -f /tmp/redaxo5.zip
-    echo >&2 "REDAXO has been successfully copied to ${PWD}"
+    echo >&2 "REDAXO has been successfully copied to $PWD"
 
     # copy default config
     cp -f /tmp/default.config.yml ./redaxo/src/core/
     rm -f /tmp/default.config.yml
-    echo >&2 "default.config.yml copied to ${PWD}/redaxo/src/core/"
+    echo >&2 "default.config.yml copied to $PWD/redaxo/src/core/"
 
     # copy setup script
     cp -f /tmp/redaxo.setup.php ./redaxo/
     rm -f /tmp/redaxo.setup.php
-    echo >&2 "redaxo.setup.php copied to ${PWD}/redaxo/"
+    echo >&2 "redaxo.setup.php copied to $PWD/redaxo/"
 
     # run setup script
     cd redaxo && php redaxo.setup.php --user="$REDAXO_USER" --password="$REDAXO_PASSWORD"
     rm -f redaxo.setup.php
 else
-    echo >&2 "WARNING: ${PWD} is not empty! Skip REDAXO setup."
+    echo >&2 "WARNING: $PWD is not empty! Skip REDAXO setup."
 fi
 
 exec "$@"
